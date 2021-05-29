2fb8c21535301e35215658e53d8218c706ba6f0f
SushilBajracharya <bajracharya.sushil01@gmail.com>
2021-05-29 14:07:03 +0545

    :boom::bug:  bugfix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function shadowizzar(options) {
-    let componets  = document.querySelectorAll('.shadowizzar');
+    let components  = document.querySelectorAll('.shadowizzar');
 
     if (options.shadow_type === "hard") {
         options.shadow_type = "0px";
@@ -8,8 +8,8 @@ function shadowizzar(options) {
         options.shadow_type = "15px";
     }
 
-    componets.forEach(componet => {
-        componet.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+    components.forEach(component => {
+        component.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
 
         if( options.padding) {
             component.style.padding= "1em";
